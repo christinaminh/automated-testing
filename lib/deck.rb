69ded3c24bb427eccf6617919a76e27271ef5790
Christina Minh <christina.minh@gmail.com>
2020-09-28 14:51:54 -0700

    Added tests for Deck class and spec-ed out the Deck class.

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,15 +5,31 @@ require_relative 'card'
 
 class Deck
 
-  def initialize
+  attr_reader :cards
 
+  def initialize(value_list = STANDARD_VALUES, suit_list = STANDARD_SUITS)
+    @cards = []
+    value_list.each do |value|
+      suit_list.each do |suit|
+        @cards << Card.new(value, suit)
+      end
+    end
   end
 
   def draw
     # returns a card
+    raise ArgumentError, "No cards left to draw" if @cards.empty?
+
+    return @cards.delete_at(-1)
   end
 
   def shuffle
     # shuffles the deck
+    return @card.shuffle!
+  end
+
+  def count
+    # counts cards in deck
+    return @cards.length
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,9 +1,67 @@
 require_relative 'test_helper'
 
 describe Deck do
+  let(:deck) do
+    Deck.new(STANDARD_VALUES, STANDARD_SUITS)
+  end
   # Write tests for the Deck class.
   # What should you test?
     # 1.  Expected Behavior
     # 2.  Edge cases
+  describe "You can create a Deck instance" do
+    it "can be instantiated" do
+      expect(deck).must_be_instance_of Deck
+    end
 
+    it "is created with 52 Card objects as attributes" do
+      expect(deck).must_respond_to :cards
+
+      deck.cards.each do |card|
+        expect(card).must_be_instance_of Card
+      end
+
+      expect(deck.cards.length).must_equal 52
+    end
+  end
+
+
+  describe "#shuffle" do
+    it "can call shuffle method on instance of deck" do
+      expect(deck).must_respond_to :shuffle
+    end
+  end
+
+  describe "#draw" do
+    it "removes a card and returns the removed card" do
+      # Test to ensure draw methods removes a card
+      # and returns the removed card
+      expect(deck.cards.length).must_equal 52
+
+      deck.draw
+
+      expect(deck.cards.length).must_equal 51
+      expect(deck.draw).must_be_instance_of Card
+    end
+
+    it "raises an ArgumentError if there are no cards to draw" do
+      52.times do
+        deck.draw
+      end
+
+      expect{deck.draw}.must_raise ArgumentError
+    end
+  end
+
+  describe "#count" do
+    it "returns the number of cards in the Deck" do
+      expect(deck.count).must_equal 52
+
+      # if no cards
+      52.times do
+        deck.draw
+      end
+
+      expect(deck.count).must_equal 0
+    end
+  end
 end
